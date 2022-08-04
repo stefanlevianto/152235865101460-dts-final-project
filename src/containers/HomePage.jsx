354c13a6b39967f9485c308340c443a582c3e4c6
stefanlevianto <stefanlevianto@gmail.com>
2022-08-04 16:24:18 +0700

    commit 2

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -4,11 +4,6 @@ import ResponsiveAppBar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../components/Style.css";
 import Main from "../components/Main";
-import {
-  useGetPokemonByNameQuery,
-  useGetPokemonEverythingQuery,
-} from "../services/pokemonApi";
-import { useParams } from "react-router-dom";
 
 const HomePage = () => {
   return (
